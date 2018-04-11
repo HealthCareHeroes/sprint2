aac7073aa565a2724d2609ad3580cf3e1a03de6e
Ashley Linnen <36825980+alinnen@users.noreply.github.com>
2018-04-10 22:59:42 -0400

    Formatting changes

--- src/test/java/edu/uncc/ssdi/controllers/UserControllerTest1.java
+++ src/test/java/edu/uncc/ssdi/controllers/UserControllerTest1.java
@@ -1,8 +1,6 @@
 package edu.uncc.ssdi.controllers;
-
 import edu.uncc.ssdi.service.*;
 import temp.WebConfig;
-
 import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
@@ -14,11 +12,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import java.util.ArrayList;
-import java.util.Arrays;
-
-import org.hibernate.jdbc.Expectations;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -38,114 +32,89 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import antlr.collections.List;
 import edu.uncc.ssdi.model.*;
 import edu.uncc.ssdi.repositories.*;
 
 @RunWith(MockitoJUnitRunner.class)
 @SpringBootTest
 @WebAppConfiguration
-@ContextConfiguration(classes = {WebConfig.class})
+@ContextConfiguration(classes = { WebConfig.class })
 public class UserControllerTest1 {
-	
-		@Autowired
-		private MockMvc mockMvc;
 
-	    @InjectMocks
-	    private UserController userController = new UserController();
-	    
-	    @Mock
-	    private UserService userService;
-	    
-	    @Autowired
-	    UserRepository userRepository;
-	   
-	    
-	    
-	 // This object will be magically initialized by the initFields method below.
-	    private JacksonTester<User> JsonUser;
-	 
-	    @Before
-	    public void setup1() {
-	        // Initializes the JacksonTester
-	        JacksonTester.initFields(this, new ObjectMapper());
-	    }
-	    
-	    @Before
-	    public void setup() {
-	 
-	        // Process mock annotations
-	        MockitoAnnotations.initMocks(this);
+	@Autowired
+	private MockMvc mockMvc;
 
-	        this.mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
-	 
-	    }
-	    
-	    public static String asJsonString(final Object obj) {
-	        try {
-	            return new ObjectMapper().writeValueAsString(obj);
-	        } catch (Exception e) {
-	            throw new RuntimeException(e);
-	        }
-	    }
-	    
-	    @Test
-		public void testAddUser() throws Exception{  
-	    		
-	    		User user = new User(10002L, "mpq@test.com", "pwd123", "patient", "Zoe", "Duan","male", "20", "9201", "APT315", "Charlotte", "NC", "28223", "704123456");      
-	    		
-	    		User savedUser = userService.saveUser(user);
-	    		
-	    		when(userController.addNewUser("abc@test.com", "pwd123", "Zoe", "Duan")).thenReturn(savedUser);
-	    	
-	    		
-	    		mockMvc.perform(MockMvcRequestBuilders.post("/adduser/")
-					.contentType(MediaType.APPLICATION_JSON)
-					.param("email", "abc@test.com")
-					.param("password", "pwd123")
-					.param("firstName", "Zoe")
-					.param("lastName", "Duan")
-					.accept(MediaType.APPLICATION_JSON))
-					.andExpect(status().isOk());			
-			
+	@InjectMocks
+	private UserController userController = new UserController();
+
+	@Mock
+	private UserService userService;
+
+	@Autowired
+	UserRepository userRepository;
+
+	// This object will be magically initialized by the initFields method below.
+	private JacksonTester<User> JsonUser;
+
+	@Before
+	public void setup1() {
+		// Initializes the JacksonTester
+		JacksonTester.initFields(this, new ObjectMapper());
+	}
+
+	@Before
+	public void setup() {
+		// Process mock annotations
+		MockitoAnnotations.initMocks(this);
+		this.mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+	}
+
+	public static String asJsonString(final Object obj) {
+		try {
+			return new ObjectMapper().writeValueAsString(obj);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
 		}
-	    	    
-	    
-	    @Test
-	    public void testUpdateUser() throws Exception {
-	        User user = new User(10002L, "mpq@test.com", "pwd123", "patient", "Zoe", "Duan","male", "20", "9201", "APT315", "Charlotte", "NC", "28223", "704123456");
-	        when(userService.findById(user.getId())).thenReturn(user);
-	        doNothing().when(userService).updateUser(user);
-	       
-	        mockMvc.perform(
-	                put("/updateUser/{id}", user.getId())
-	                        .contentType(MediaType.APPLICATION_JSON)
-	                        .content(asJsonString(user)))
-	                .andExpect(status().isOk());
-	        verify(userService, times(1)).findById(user.getId());
-	        verify(userService, times(1)).updateUser(user);
-	        verifyNoMoreInteractions(userService);
-	    }
-	    
-	    
-	   @Test
-	    public void testGetAllUsers() throws Exception {
-	    		ArrayList<User> expected = new ArrayList<User>();
-	    			expected.add(new User(10002L, "mpq@test.com", "pwd123", "doctor", "Zoe", "Duan","male", "20", "9201", "APT315", "Charlotte", "NC", "28223", "704123456"));
-	    			expected.add(new User(10003L, "abc@test.com", "pwd123", "doctor", "Joe", "Smith","male", "20", "9201", "APT315", "Charlotte", "NC", "28223", "704123456"));
-	        
-	    			when(userService.findAllUsers()).thenReturn(expected);
+	}
 
-	       
-	        mockMvc.perform(get("/getUsers"))
-	                .andExpect(status().isOk())
-	                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-	                .andExpect(jsonPath("$[0].id", is(10002)))
-	                .andExpect(jsonPath("$[0].email", is("mpq@test.com")))
-	                .andExpect(jsonPath("$[1].id", is(10003)))
-	                .andExpect(jsonPath("$[1].role", is("doctor")));
-	        verify(userService, times(1)).findAllUsers();
-	        verifyNoMoreInteractions(userService);
-	    }
-	
-}
+	@Test
+	public void testAddUser() throws Exception {
+		User user = new User(10002L, "mpq@test.com", "pwd123", "patient", "Zoe", "Duan", "male", "20", "9201", "APT315",
+				"Charlotte", "NC", "28223", "704123456");
+		User savedUser = userService.saveUser(user);
+		when(userController.addNewUser("abc@test.com", "pwd123", "Zoe", "Duan")).thenReturn(savedUser);
+		mockMvc.perform(MockMvcRequestBuilders.post("/adduser/").contentType(MediaType.APPLICATION_JSON)
+				.param("email", "abc@test.com").param("password", "pwd123").param("firstName", "Zoe")
+				.param("lastName", "Duan").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+
+	}
+
+	@Test
+	public void testUpdateUser() throws Exception {
+		User user = new User(10002L, "mpq@test.com", "pwd123", "patient", "Zoe", "Duan", "male", "20", "9201", "APT315",
+				"Charlotte", "NC", "28223", "704123456");
+		when(userService.findById(user.getId())).thenReturn(user);
+		doNothing().when(userService).updateUser(user);
+		mockMvc.perform(put("/updateUser/{id}", user.getId()).contentType(MediaType.APPLICATION_JSON)
+				.content(asJsonString(user))).andExpect(status().isOk());
+		verify(userService, times(1)).findById(user.getId());
+		verify(userService, times(1)).updateUser(user);
+		verifyNoMoreInteractions(userService);
+	}
+
+	@Test
+	public void testGetAllUsers() throws Exception {
+		ArrayList<User> expected = new ArrayList<User>();
+		expected.add(new User(10002L, "mpq@test.com", "pwd123", "doctor", "Zoe", "Duan", "male", "20", "9201", "APT315",
+				"Charlotte", "NC", "28223", "704123456"));
+		expected.add(new User(10003L, "abc@test.com", "pwd123", "doctor", "Joe", "Smith", "male", "20", "9201",
+				"APT315", "Charlotte", "NC", "28223", "704123456"));
+		when(userService.findAllUsers()).thenReturn(expected);
+		mockMvc.perform(get("/getUsers")).andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+				.andExpect(jsonPath("$[0].id", is(10002))).andExpect(jsonPath("$[0].email", is("mpq@test.com")))
+				.andExpect(jsonPath("$[1].id", is(10003))).andExpect(jsonPath("$[1].role", is("doctor")));
+		verify(userService, times(1)).findAllUsers();
+		verifyNoMoreInteractions(userService);
+	}
+} // end of class
--- src/test/java/edu/uncc/ssdi/service/UserServiceTest.java
+++ src/test/java/edu/uncc/ssdi/service/UserServiceTest.java
@@ -1,19 +1,13 @@
 package edu.uncc.ssdi.service;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
-import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.when;
-
-import java.util.Date;
 
 import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,81 +15,63 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.transaction.annotation.Transactional;
-
 import edu.uncc.ssdi.model.User;
 import edu.uncc.ssdi.repositories.*;
-
 
 @RunWith(MockitoJUnitRunner.class)
 @SpringBootTest
 public class UserServiceTest {
-	
 	@Autowired
 	private MockMvc mockMvc;
-	
+
 	private UserServiceImpl userServiceImpl;
-	
-   @Bean
+
+	@Bean
 	public UserRepository todoService() {
-	        return Mockito.mock(UserRepository.class);}
-	
+		return Mockito.mock(UserRepository.class);
+	}
+
 	@Bean
 	private Systems todoService1() {
-        return Mockito.mock(Systems.class);}
-	
-	
+		return Mockito.mock(Systems.class);
+	}
+
 	@Mock
 	private Systems sysObject;
-	
+
 	@Mock
 	public UserRepository userRepository;
-	
-	
+
 	@Mock
 	private User testUser;
-	
-	//@InjectMocks
-	
+	// @InjectMocks
+
 	@Before
-	public void setUp()
-	{
-	    MockitoAnnotations.initMocks(this);
-	    userServiceImpl = new UserServiceImpl();
-	    userServiceImpl.setUserRepository(userRepository);	    
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+		userServiceImpl = new UserServiceImpl();
+		userServiceImpl.setUserRepository(userRepository);
 	}
-	
 	// Testing Data Field
 	long id = 10000;
 
-	
 	@Test
 	public void testSaveUser() {
-		
-        // Arrange       
-        when(userRepository.save(testUser)).thenReturn(testUser);
-        // Act         
-        User savedUser = userServiceImpl.saveUser(testUser);
-        //// Assert       
-        
-        assertEquals(savedUser, testUser);
-       
-    }
-	
-	
+		// Arrange
+		when(userRepository.save(testUser)).thenReturn(testUser);
+		// Act
+		User savedUser = userServiceImpl.saveUser(testUser);
+		// Assert
+		assertEquals(savedUser, testUser);
+	}
+
 	@Test
 	public void testUserfindById() {
-		// Arrange     
-        when(userRepository.findOne(id)).thenReturn(testUser);
-        // Act    
-        User user = userServiceImpl.findById(id);
-        // Assert    
-        assertEquals(user, testUser);
-		
+		// Arrange
+		when(userRepository.findOne(id)).thenReturn(testUser);
+		// Act
+		User user = userServiceImpl.findById(id);
+		// Assert
+		assertEquals(user, testUser);
 	}
-		
-	
-	
-	
-}
+} // end of class
