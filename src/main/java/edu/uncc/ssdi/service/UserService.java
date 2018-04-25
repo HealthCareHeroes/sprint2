78ec6169e5700cbb4a2f2412454ab29ef5cc7869
Ashley Linnen <36825980+alinnen@users.noreply.github.com>
2018-04-25 14:31:31 -0400

    Commit Ashley working on request access

--- src/main/java/edu/uncc/ssdi/service/UserService.java
+++ src/main/java/edu/uncc/ssdi/service/UserService.java
@@ -1,5 +1,4 @@
 package edu.uncc.ssdi.service;
-
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -9,7 +8,6 @@ import edu.uncc.ssdi.model.User;
 
 @Service
 public interface UserService {
-	
 	User findById(Long id);
 
 	List<User> findByEmail(String email);
@@ -33,4 +31,8 @@ public interface UserService {
 	User validateUser(Login login);
 
 	User validateEmail(String email);
+	
+	// request access to a patient's medical history
+	void requestAccess();
+	
 } // end of class
--- src/main/java/edu/uncc/ssdi/service/UserServiceImpl.java
+++ src/main/java/edu/uncc/ssdi/service/UserServiceImpl.java
@@ -88,4 +88,15 @@ public class UserServiceImpl implements UserService {
 		// TODO Auto-generated method stub
 		return userRepository.findByRole(role);
 	}
+	
+	// request access to a patient's medical history
+	public void requestAccess() {
+		/* When "Request Access" button is clicked on "Patients" page,
+		 *  
+		 */
+		
+	}
+	
+	
+	
 } // end of class
